8782e8e7a1aea9804561c31bc0156ece2f13d359
Thawinwats Uggaravisee <thawinwats.ugg@satangcorp.com>
2023-08-28 15:50:39 +0700

    🧪 Test redirectToStore

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from "react"
+import { isMobile, isIOS } from "react-device-detect"
+
+const appScheme = "satangpro://login?" // Replace with your app's deep link URL
+const appStoreUrls = {
+  ios: "https://apps.apple.com/us/app/satang-pro/id1513155132", // Replace with your app's App Store URL for iOS
+  android: "https://play.google.com/store/apps/details?id=com.satang.pro", // Replace with your app's Google Play Store URL for Android
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  useEffect(() => {
+    const redirectToAppOrStore = () => {
+      const timeout = setTimeout(() => {
+        redirectToStore()
+      }, 3000) // Wait for 3 seconds before redirecting to the app store
+
+      window.location.href = appScheme
+
+      // Clear the timeout if the deep link successfully opens the app
+      window.addEventListener("visibilitychange", () => {
+        if (document.hidden) {
+          clearTimeout(timeout)
+        }
+      })
+    }
+
+    if (isMobile) {
+      redirectToAppOrStore()
+    }
+  }, [])
+
+  const redirectToStore = () => {
+    const storeLink = isIOS ? appStoreUrls.ios : appStoreUrls.android
+    window.location.href = storeLink
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="bg-slate-950 flex flex-col justify-center items-center h-screen gap-4">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
   )
 }
 
